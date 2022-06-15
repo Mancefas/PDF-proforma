933cbe8adb2ee5af363e0798c4af0d05bed9b580
Mancefas <mantvydasliobe@gmail.com>
2022-06-15 14:39:15 +0300

    added jsPDF dependency to make PDF document

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import "./App.css";
+import ReactDOMServer from "react-dom/server";
+import jsPDF from "jspdf";
+const doc = new jsPDF();
+const Foo = <b>foo</b>;
 
 function App() {
-  return <div className="App"></div>;
+  const save = () => {
+    doc.html(ReactDOMServer.renderToStaticMarkup(Foo), {
+      callback: () => {
+        doc.save("myDocument.pdf");
+      },
+    });
+  };
+
+  return (
+    <div>
+      <button onClick={save}>save</button>
+    </div>
+  );
 }
 
 export default App;
